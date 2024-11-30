2a95c4fb904567293bc91431151d681599647240
Eng-mostafa-osama <mostafaosama3060@gmail.com>
2024-12-01 00:43:26 +0200

    Update Main.java

--- main/src/application/Main.java
+++ main/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
@@ -11,7 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1280,720,Color.RED); 
+			primaryStage.setTitle("AL Ahly SC");// title of the application
+			
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
