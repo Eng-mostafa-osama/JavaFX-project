3273a25b17172d8d6a0390eb72cc0b31aea91d91
Eng-mostafa-osama <mostafaosama3060@gmail.com>
2024-12-01 19:37:26 +0200

    basics

--- main/src/application/Main.java
+++ main/src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
@@ -11,12 +14,17 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene homeScreen = new Scene(root,1280,720,Color.RED); 
+			Parent root = FXMLLoader.load(getClass().getResource("/home_page.fxml"));
+			Scene homeScreen = new Scene(root); 
 			primaryStage.setTitle("AL Ahly SC");// title of the application
 			// set global css sheet 
 			String CSS = this.getClass().getResource("application.css").toExternalForm();
 			homeScreen.getStylesheets().add(CSS);
+			
+			
+			Image icon = new Image("file:ahly Logo.png");
+			primaryStage.getIcons().add(icon);
+			
 			
 			primaryStage.setScene(homeScreen);
 			primaryStage.show();
